8a109c84c65928680defd81924b851730ec12e3a
Thibault Ayanides <thibault.ayanides@skale-5.com>
2023-08-14 11:52:10 +0200

    feat: downgrade to python3.10

--- src/app/constants.py
+++ src/app/constants.py
@@ -1,4 +1,4 @@
 import os
 
-APP_NAME = os.environ.get("APP_NAME", "rideshare")
+APP_NAME = os.environ.get("APP_NAME", "fastapi-profiling-example")
 PYROSCOPE_ENDPOINT = os.environ.get("PYROSCOPE_ENDPOINT", "http://pyroscope.ayanides.cloud")
